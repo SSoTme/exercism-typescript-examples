63233f3c55bf94375b375392111d5c63a3f323dc
eejai42 <start@anabstractlevel.com>
2019-09-29 15:36:14 -0500

    Improved submit a bit on windows.

--- space-age/submit.bat
+++ space-age/submit.bat
@@ -1,1 +1,3 @@
-exercism submit ./space-age.ts ./space-age-base.ts ./planet-orbits.json ./write-raw/create-raw-solution.xslt
+call yarn test
+call exercism submit ./space-age.ts ./space-age-base.ts ./planet-orbits.json ./write-raw/create-raw-solution.xslt
+pause
