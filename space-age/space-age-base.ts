f9def42147f04fb0d5ea33fa1c55dd354a7ae10c
eejai42 <start@anabstractlevel.com>
2019-09-29 17:46:23 -0500

    Minor tweaks to config - more testing.

--- space-age/space-age-base.ts
+++ space-age/space-age-base.ts
@@ -1,11 +1,11 @@
 class SpaceAgeBase {
     public seconds : number;
 
-    constructor(spaceAgeInSeconds:number) {
+    constructor(spaceAgeInSeconds : number) {
         this.seconds = spaceAgeInSeconds;
     }
 
-    toPlanet(earthRelativeOrbit:number) {
+    toPlanet(earthRelativeOrbit : number) {
         return Number((this.seconds / earthRelativeOrbit / 365.25 / 24 / 60 / 60).toFixed(2));
     }
 }
